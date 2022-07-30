139fc88a1bc7a1adad9688d8d22052459e5916b1
Tommy NgX <58539340+Tommy-Ngx@users.noreply.github.com>
2022-07-30 13:09:22 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -2,24 +2,52 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
+#procfile.txt 
+#web: gunicorn app:app
+#first file that we have to run first : flask server name
 app = Flask(__name__)
+pkl_file = open('model.pkl','rb')
 model = pickle.load(open('model.pkl', 'rb'))
+index_dict = pickle.load(pkl_file)
+
 
 @app.route('/')
 def home():
-    return render_template('index1.html')
+    return render_template('index.html')
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    output = round(prediction[0], 2)
 
-    return render_template('index1.html', prediction_text='The predicted cost of house is $ {}'.format(output))
+    if request.method=='POST':
+        result = request.form
+
+        index_dict = pickle.load(open('cat','rb'))
+        location_cat = pickle.load(open('location_cat','rb'))
+
+        new_vector = np.zeros(151)
+
+        result_location = result['location']
+
+        if result_location not in location_cat:
+            new_vector[146] = 1
+        else:
+            new_vector[index_dict[str(result['location'])]] = 1
+
+
+        new_vector[index_dict[str(result['area'])]] = 1
+
+        new_vector[0] = result['sqft']
+        new_vector[1] = result['bath']
+        new_vector[2] = result['balcony']
+        new_vector[3] = result['size']
+
+    new = [new_vector]
+
+    prediction = model.predict(new)
+    #print(prediction)
+
+    return render_template('index.html', Predict_score ='Your house estimate price is  ₹ {} lakhs'.format(prediction))
 
 if __name__ == "__main__":
     app.run(debug=True)
